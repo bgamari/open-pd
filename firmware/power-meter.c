7e0715a0f80af6f910777a1dfc8790930bdbe075
Ben Gamari <ben@smart-cactus.org>
2015-01-30 08:38:34 -0500

    firmware: Increase max autoscale threshold

--- firmware/power-meter.c
+++ firmware/power-meter.c
@@ -148,7 +148,7 @@ static bool autoscale = false;
 // Minimum tolerable voltage before increasing gain in microvolts
 static uint32_t autoscale_min_thresh = 0.05 * 1e6;
 // Maximum tolerable voltage before decreasing gain in microvolts
-static uint32_t autoscale_max_thresh = 3.2 * 1e6;
+static uint32_t autoscale_max_thresh = 3.25 * 1e6;
 static enum range active_range;
 
 void set_range(enum range rng) {
