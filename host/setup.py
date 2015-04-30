d440ad032cb04d2b7132895c124f40213c245f4a
Kieran Ramos <ramos.kieran@gmail.com>
2015-04-30 16:42:58 -0400

    Enable os dependent install to handle services on different systems

--- host/setup.py
+++ host/setup.py
@@ -1,17 +1,46 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
 
-setup(name='openpd',
-      version='1.0',
-      description='Library for interfacing with OpenPD laser power meter',
-      author='Ben Gamari',
-      author_email='ben@smart-cactus.org',
-      url='https://www.github.com/bgamari/open-pd',
-      py_modules=['openpd'],
-      scripts=['openpd', 'openpd-daemon', 'plot-power'],
-      data_files=[
-          ('/etc/udev/rules.d', ['99-openpd.rules']),
-          ('/lib/systemd/system', ['openpd-daemon.service']),
-      ],
-     )
+if os.sys.platform == 'posix':
+    setup(name='openpd',
+          version='1.0',
+          description='Library for interfacing with OpenPD laser power meter',
+          author='Ben Gamari',
+          author_email='ben@smart-cactus.org',
+          url='https://www.github.com/bgamari/open-pd',
+          py_modules=['openpd'],
+          scripts=['openpd', 'openpd-daemon', 'plot-power'],
+          data_files=[
+              ('/etc/udev/rules.d', ['99-openpd.rules']),
+              ('/lib/systemd/system', ['openpd-daemon.service']),
+          ],
+         )
+    
+elif os.sys.platform == 'win32':
+    from distutils.command.install import install
+
+    class Install(install):
+        def run(self):
+            install.run(self)
+
+            # post-install
+            from subprocess import check_call
+            import os
+            
+            winsvc_path = os.path.join(self.install_base,'Scripts',
+                                       'openpd-winsvc.py')
+            check_call(['python',winsvc_path,'--startup','auto','install'])
+            check_call(['python',winsvc_path,'start'])
+    
+    setup(name='openpd',
+          version='1.0',
+          description='Library for interfacing with OpenPD laser power meter',
+          author='Ben Gamari',
+          author_email='ben@smart-cactus.org',
+          url='https://www.github.com/bgamari/open-pd',
+          py_modules=['openpd'],
+          scripts=['openpd', 'plot-power', 'openpd-winsvc.py'],
+          cmdclass={'install': Install},
+         )
